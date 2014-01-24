24d0aa3574b554bca1167ecf263700e8a5dfa18e
Muslu YÜKSEKTEPE <musluyuksektepe@gmail.com>
2014-01-24 17:15:49 +0200

    Create settings.py
    settings.py de tanımlanan parametreler

---
+++ settings.py
@@ -0,0 +1,9 @@
+CLIENTID	= 	"700200000"  # Banka tarafindan verilen isyeri numarasi
+ 
+OKURL 		= 	"http://127.0.0.1:8000/payodeme/"    # Islem basariliysa dönülecek isyeri sayfasi  (3D isleminin ve ödeme isleminin sonucu)
+
+FAILURL 	= 	"http://127.0.0.1:8000/payodeme/"  # Islem basarizsa dönülecek isyeri sayfasi  (3D isleminin ve ödeme isleminin sonucu)
+
+ISLEMTIPI	=	"Auth"     # Islem tipi
+
+STOREKEY 	= 	"123456"  # isyeri anahtari
